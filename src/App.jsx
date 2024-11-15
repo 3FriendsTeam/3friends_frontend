1d003d72a211a29b6cd2337490528bf36d31f5d4
Huy Huy <muonnvtabth03@gmail.com>
2024-11-15 14:13:29 +0700

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { EmployeeAuthProvider } from './AuthContext/EmployeeAuthContext';
 import EmployeeRoutes from './pages/admin/Employee.Routes';
-
 import { CustomerAuthProvider } from './AuthContext/CustomerAuthContext';
 import CustomerRoutes from './pages/customer/Customer.Routes';
 
 
 function App() {
   return (
-    
       <CustomerAuthProvider>
         <EmployeeAuthProvider>
           <Routes>

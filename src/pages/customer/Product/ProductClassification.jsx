b8e69e1bc0edf8797654c46ed7f3c8f0f8d7c00d
hung3107 <hunghua3107@gmail.com>
2024-11-28 17:11:39 +0700

    a

--- src/pages/customer/Product/ProductClassification.jsx
+++ src/pages/customer/Product/ProductClassification.jsx
@@ -36,83 +36,79 @@ const ProductClassification = ({ categoryId, onSortChange }) => {
   }, [categoryId]);
 
   return (
-<div className="p-6 w-full max-w-[1536px] mx-auto flex justify-center bg-[#F2F2F2] -mb-12">
-  <div className="w-full lg:w-[1170px] bg-white rounded-2xl">
-    {/* Tiêu đề và số sản phẩm */}
-    <div className="flex items-center gap-4 ml-6 mt-4">
+    <div className="p-6 w-full max-w-[1536px] mx-auto flex justify-center bg-[#F2F2F2] -mb-12">
+      <div className="w-full lg:w-[1170px] bg-white rounded-2xl">
+        {/* Tiêu đề và số sản phẩm */}
+        {/* <div className="flex items-center gap-4 ml-6 mt-4">
       <h1 className="text-2xl font-bold mb-2 text-[#333]">Điện thoại</h1>
       <p className="text-gray-600">{productCount} sản phẩm</p>
-    </div>
+    </div> */}
 
-    {/* Bộ lọc: Hãng sản xuất */}
-    <p className="ml-6 font-bold text-[#333]">Hãng sản xuất</p>
-    <div className="flex flex-wrap mt-4 mb-6 ml-6">
-      <div className="flex items-center space-x-2">
-        {images.map((src, index) => (
-          <img key={index} src={src} alt={`${index + 1}`} />
-        ))}
+        {/* Bộ lọc: Hãng sản xuất */}
+        <p className="mt-6 ml-6 font-bold text-[#333]">Hãng sản xuất</p>
+        <div className="flex flex-wrap mt-4 mb-6 ml-6">
+          <div className="flex items-center space-x-2">
+            {images.map((src, index) => (
+              <img key={index} src={src} alt={`${index + 1}`} />
+            ))}
+          </div>
+        </div>
+
+        {/* Bộ lọc: Mức giá, Màn hình, Camera, v.v. */}
+        <div className="flex flex-col mb-4 ml-6 text-[#333]">
+          <span className="mr-4 font-bold">Lọc theo</span>
+          <div className="flex flex-wrap mt-2 text-[14px]">
+            <p className="rounded-md px-3 py-1 mr-2 flex items-center gap-1">
+              <icons.FaFilter />
+              Mức giá:
+            </p>
+            <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
+              Dưới 5 triệu
+            </button>
+            <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
+              Từ 5 - 10 triệu
+            </button>
+            <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
+              Từ 10 - 15 triệu
+            </button>
+            <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
+              Trên 15 triệu
+            </button>
+          </div>
+        </div>
+
+        {/* Sắp xếp theo */}
+        <div className="flex flex-col mb-8 ml-6 text-[#333]">
+          <span className="mr-4 font-bold">Sắp xếp theo</span>
+          <div className="flex flex-wrap mt-2 text-[14px]">
+            <button
+              className="border border-gray-200 rounded-md px-3 py-1 mr-2"
+              onClick={() => onSortChange("newest")}
+            >
+              Mới nhất
+            </button>
+            <button
+              className="border border-gray-200 rounded-md px-3 py-1 mr-2"
+              onClick={() => onSortChange("sold")}
+            >
+              Bán chạy
+            </button>
+            <button
+              className="border border-gray-200 rounded-md px-3 py-1 mr-2"
+              onClick={() => onSortChange("price_high")}
+            >
+              Giá tăng
+            </button>
+            <button
+              className="border border-gray-200 rounded-md px-3 py-1 mr-2"
+              onClick={() => onSortChange("price_low")}
+            >
+              Giá giảm
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-
-    {/* Bộ lọc: Mức giá, Màn hình, Camera, v.v. */}
-    <div className="flex flex-col mb-4 ml-6 text-[#333]">
-      <span className="mr-4 font-bold">Lọc theo</span>
-      <div className="flex flex-wrap mt-2 text-[14px]">
-        <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
-          <icons.FaFilter />
-          Mức giá
-        </button>
-        <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
-          Màn hình
-          <icons.IoMdArrowDropdown />
-        </button>
-        <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
-          Camera sau
-          <icons.IoMdArrowDropdown />
-        </button>
-        <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
-          Hệ điều hành
-          <icons.IoMdArrowDropdown />
-        </button>
-        <button className="border border-gray-200 rounded-md px-3 py-1 mr-2 flex items-center gap-1">
-          Tính năng đặc biệt
-          <icons.IoMdArrowDropdown />
-        </button>
-      </div>
-    </div>
-
-    {/* Sắp xếp theo */}
-    <div className="flex flex-col mb-8 ml-6 text-[#333]">
-      <span className="mr-4 font-bold">Sắp xếp theo</span>
-      <div className="flex flex-wrap mt-2 text-[14px]">
-        <button
-          className="border border-gray-200 rounded-md px-3 py-1 mr-2"
-          onClick={() => onSortChange("newest")}
-        >
-          Mới nhất
-        </button>
-        <button
-          className="border border-gray-200 rounded-md px-3 py-1 mr-2"
-          onClick={() => onSortChange("sold")}
-        >
-          Bán chạy
-        </button>
-        <button
-          className="border border-gray-200 rounded-md px-3 py-1 mr-2"
-          onClick={() => onSortChange("price_high")}
-        >
-          Giá tăng
-        </button>
-        <button
-          className="border border-gray-200 rounded-md px-3 py-1 mr-2"
-          onClick={() => onSortChange("price_low")}
-        >
-          Giá giảm
-        </button>
-      </div>
-    </div>
-  </div>
-</div>
 
   );
 };
